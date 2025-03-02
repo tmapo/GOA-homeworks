5cd5d0ce7aba9960bb2d69c97bab0240729c381c
asanidzeapolon732@gmail.com <asanidzeapolon732@gmail.com>
2025-03-02 18:08:51 +0400

    second commit

---
+++ GOA-lessons/day 06/classwork/lesson 6.py
@@ -0,0 +1,5 @@
+#ask the user for their name and surename
+
+name = input("enter your name: ")
+surename = input("enter your surename: ")
+print(name , surename)
---
+++ GOA-lessons/day 08/claswork/lesson 8.py
@@ -0,0 +1,18 @@
+#>
+print(7 > 3)
+print(9 > 4)
+#<
+print(4 < 9) 
+print(2 < 5)
+#>=
+print(5 >= 5)
+print(10 >= 7)
+#<=
+print(8 <= 10)
+print(5 <= 5)
+#==
+print(12 == 12)
+print(6 == 6)
+#!=
+print(15 != 18)
+print(9 != 10)
